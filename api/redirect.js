2498d6cef10b9f7ad68fddada6e2fe75c1273656
mporwal-hash <155471010+mporwal-hash@users.noreply.github.com>
2025-11-06 11:32:35 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,16 +1,9 @@
-import fs from "fs";
-import path from "path";
-
-/**
- * Example mapping for short codes.
- * You could also store this in a DB or external JSON.
- */
 const shortLinks = {
-  "Scholrageis": { brand: "ScholrAgeis", path: "/ScholrAgeis/home" },
+  "scholrageis": { brand: "ScholrAgeis", path: "/ScholrAgeis/home" },
 };
 
 export default async function handler(req, res) {
-  const code = req.query.code;
+  const code = (req.query.code || '').toLowerCase();
 
   if (!code || !shortLinks[code]) {
     res.status(404).send("Invalid or missing code");
@@ -18,8 +11,6 @@ export default async function handler(req, res) {
   }
 
   const { brand, path: deepPath } = shortLinks[code];
-
-  // Construct final deep link
   const redirectUrl = `https://${req.headers.host}${deepPath}`;
 
   res.writeHead(302, { Location: redirectUrl });
